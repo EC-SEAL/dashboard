c62ec666de90032caec12a83c46abd6b97fb6722
Raúl <39159853+Raul14@users.noreply.github.com>
2021-06-16 13:17:42 +0200

    New GUI for Web Dashboard

--- urls.py
+++ urls.py
@@ -7,29 +7,33 @@ from . import controlFunctions
 urlpatterns = [
     path('', views.index, name='index'),
 
-    re_path(r'^tokenFlag=(?P<UUID>[a-z0-9]{32})$', csrf_exempt(controlFunctions.tokenFlag)),
-    re_path(r'^identitymanager/tokenFlag=(?P<UUID>[a-z0-9]{32})$', csrf_exempt(controlFunctions.tokenFlag)),
-    re_path(r'^tokenValidate=(?P<UUID>[a-z0-9]{32})$', csrf_exempt(controlFunctions.tokenControl)),
+    path('logout', views.logout, name='logout'),
 
-    path('loadLocalPDS/', csrf_exempt(useCases.uc1_02)),
-    path('loadCloudPDS/', csrf_exempt(useCases.uc1_03)),
-    path('SSI/', csrf_exempt(useCases.uc1_04)),
+    # re_path(r'^tokenFlag=(?P<UUID>[a-z0-9]{32})$', csrf_exempt(controlFunctions.tokenFlag)),
+    # re_path(r'^identitymanager/tokenFlag=(?P<UUID>[a-z0-9]{32})$', csrf_exempt(controlFunctions.tokenFlag)),
+    # re_path(r'^tokenValidate=(?P<UUID>[a-z0-9]{32})$', csrf_exempt(controlFunctions.tokenControl)),
 
-    path('identitymanager/', views.identity_manager, name='identity_manager'),
+    # path('loadLocalPDS/', csrf_exempt(useCases.uc1_02)),
+    # path('loadCloudPDS/', csrf_exempt(useCases.uc1_03)),
+    # path('SSI/', csrf_exempt(useCases.uc1_04)),
 
-    path('identitymanager/storeLocalPDS/', csrf_exempt(useCases.uc2_02)),
-    path('identitymanager/storeCloudPDS/', csrf_exempt(useCases.uc2_05)),
+    #path('identitymanager/', views.identity_manager, name='identity_manager'),
 
-    path('identitymanager/addID/', csrf_exempt(useCases.uc3_02)),
+    # path('identitymanager/storeLocalPDS/', csrf_exempt(useCases.uc2_02)),
+    # path('identitymanager/storeCloudPDS/', csrf_exempt(useCases.uc2_05)),
 
-    path('identitymanager/vcIssue/', csrf_exempt(useCases.uc5_01)),
+    # path('identitymanager/addID/', csrf_exempt(useCases.uc3_02)),
 
-    path('identitymanager/idDeriv/', csrf_exempt(useCases.uc6_01)),
+    # path('identitymanager/vcIssue/', csrf_exempt(useCases.uc5_01)),
 
-    path('identitymanager/idRecon/', csrf_exempt(useCases.uc7_01)),
-    path('identitymanager/idSelec/', csrf_exempt(useCases.uc0_01)),
+    # path('identitymanager/idDeriv/', csrf_exempt(useCases.uc6_01)),
 
-    re_path(r'^identitymanager/manageidentitydata=(?P<UUID>[a-z0-9]{32})$', views.manageidentity, name='manageidentity'),
+    # path('identitymanager/idRecon/', csrf_exempt(useCases.uc7_01)),
+    # path('identitymanager/idSelec/', csrf_exempt(useCases.uc0_01)),
+
+    path('manageidentitydata/', views.manageidentity, name='manageidentity'),
+    
+    #re_path(r'^identitymanager/manageidentitydata=(?P<UUID>[a-z0-9]{32})$', views.manageidentity, name='manageidentity'),
 
     re_path(r'^testGetSessionId=(?P<UUID>[a-z0-9]{32})$', csrf_exempt(controlFunctions.debugGetSessionId)),
 ]
