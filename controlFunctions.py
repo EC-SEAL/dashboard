3ce3014861069356e0a0ce0fc5c670795fe808b7
Raúl <39159853+Raul14@users.noreply.github.com>
2021-06-18 12:49:49 +0200

    URL callback redirection fix

--- controlFunctions.py
+++ controlFunctions.py
@@ -278,7 +278,7 @@ def tokenControl(request, UUID):
     #if request.POST.get('msToken', None):
     if request.POST:
 
-        return HttpResponseRedirect('dashboard/tokenValidate='+UUID)
+        return HttpResponseRedirect('https://seal.uma.es/dashboard/tokenValidate='+UUID)
 
         # try:
         #     token = request.POST.get('msToken')
